0a9e9082572d989c78d08c1ed35ac81bfd9c6d1a
Cristhiam Reina <cristiansrc@gmail.com>
2020-11-14 18:25:16 -0500

    Commit inicial

---
+++ src/main/java/com/cristhiam/reina/challengebefuegoq/ChallengebefuegoqApplication.java
@@ -0,0 +1,13 @@
+package com.cristhiam.reina.challengebefuegoq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengebefuegoqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengebefuegoqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cristhiam/reina/challengebefuegoq/ChallengebefuegoqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristhiam.reina.challengebefuegoq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengebefuegoqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
